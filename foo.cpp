a752e54d1d51d1bb299f5ba925b4a550ea638356
Miguel Prada <miguel.prada@tecnalia.com>
2018-01-25 17:34:46 +0100

    No error

--- foo.cpp
+++ foo.cpp
@@ -1,4 +1,17 @@
-#include "foo.h"
+#include <boost/shared_ptr.hpp>
+
+class Bar;
+
+class Foo
+{
+public:
+  Foo();
+  int getA();
+  int getB();
+private:
+  int a;
+  boost::shared_ptr<Bar> bar;
+};
 
 class Bar
 {
